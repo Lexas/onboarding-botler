47c87baee7a955c1b3779de93c8960ec674530e3
lexas <dislexas@gmail.com>
2015-11-18 17:39:31 -0700

    child_process test

--- server.js
+++ server.js
@@ -3,6 +3,7 @@
 
 var debug = require('debug')('botler')
 var express = require('express')
+var exec = require('child_process').exec
 var app = express()
 
 app.use(require('serve-static')(__dirname +'/public'))
@@ -10,5 +11,13 @@ app.use(require('serve-static')(__dirname +'/public'))
 var bodyParser = require('body-parser')
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended: false}))
+
+app.route('/addToRepo')
+	.get(function(req, res){
+		exec('echo hello', function(err, stdout, stderr){
+			console.log('stdout: %s', stdout);	
+		});
+		res.send('yay');
+	})
 
 app.listen(3000)
